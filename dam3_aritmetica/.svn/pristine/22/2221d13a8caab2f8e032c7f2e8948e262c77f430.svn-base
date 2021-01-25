0f42484a5a79f48a0461ddd9b4876b8dfe045239
noemisma <noemisma@outlook.es>
2021-01-25 07:37:35 +0100

    Repositorios

---
+++ dam3_aritmetica/.svn/pristine/22/2221d13a8caab2f8e032c7f2e8948e262c77f430.svn-base
@@ -0,0 +1,30 @@
+package com.dam.aritmetica;
+
+public class OperacionesBasicas {
+
+	public static int suma(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int resta(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public static int multiplicacion(int a, int b)
+	{
+		return a*b;
+	}
+	
+	public static int division(int a, int b)
+	{
+		return a/b;
+	}
+	
+	public static int areaTriangulo(int a, int b)
+	{
+		return (a*b)/2;
+	}
+	
+}
---
+++ dam3_calculadora/src/main/java/com/dam/calculadora/Main.java
@@ -0,0 +1,60 @@
+package com.dam.calculadora;
+
+import java.util.Scanner;
+
+import com.dam.aritmetica.OperacionesBasicas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("CALCULADORA");
+			System.out.println("1.- SUMA.");
+			System.out.println("2.- RESTA.");
+			System.out.println("3.- MULTIPLICACION");
+			System.out.println("4.- DIVISION");
+			System.out.println("7.- AREA TRIANGULO");
+			System.out.println("0.- SALIR");
+			System.out.println("INTRODUZCA OPCION");
+
+			int opcion = teclado.nextInt();
+
+			System.out.println("Introduzca valor a:");
+			int a = teclado.nextInt();
+			System.out.println("Introduzca valor b:");
+			int b = teclado.nextInt();
+
+			switch (opcion) 
+			{
+				case 1:
+					System.out.println("SUMA:");
+					System.out.println(OperacionesBasicas.suma(a, b));
+					break;
+				case 2:
+					System.out.println("RESTA:");
+					System.out.println(OperacionesBasicas.resta(a, b));
+					break;
+				case 3:
+					System.out.println("MULTIPLICACION:");
+					System.out.println(OperacionesBasicas.multiplicacion(a, b));
+					break;
+				case 4:
+					System.out.println("DIVISION:");
+					System.out.println(OperacionesBasicas.division(a, b));
+					break;
+				case 7:
+					System.out.println("AREA TRIANGULO:");
+					System.out.println(OperacionesBasicas.areaTriangulo(a, b));
+					break;
+				case 0:
+					return;
+			}
+
+		}
+
+	}
+
+}
